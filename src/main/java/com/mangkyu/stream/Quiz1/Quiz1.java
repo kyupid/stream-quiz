ad254cd561b71cb0518271b2679e8fd1a2b144f1
kyupid <iamscvv@gmail.com>
2023-01-06 23:01:18 +0900

    quiz1 for-loop

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,6 +3,7 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,22 @@ public class Quiz1 {
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] s : csvLines) {
+            String[] split = s[1].split(":");
+            for (String str : split) {
+                String trim = str.trim();
+                if (map.get(trim) == null) {
+                    map.put(trim, 1);
+                } else {
+                    Integer value = map.get(trim);
+                    value = 1 + value;
+                    map.put(trim, value);
+                }
+//                System.out.println(trim);
+            }
+        }
+        return map;
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
