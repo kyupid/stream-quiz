a74f398f6f52fe6e1472cd404e2f14fa53141716
Yeonwoo <kyu@whatap.io>
2023-01-15 11:12:23 +0900

    quiz5

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,7 +1,11 @@
 package com.mangkyu.stream.Quiz5;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Quiz5 {
 
@@ -19,8 +23,27 @@ public class Quiz5 {
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
-        return Collections.emptyList();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            list.add((int) (Math.random() * 45) + 1);
+        }
+        return list;
     }
+
+    public static void main(String[] args) {
+        IntStream ints = new Random().ints(1, 46);
+
+        // 1,2,3,4,5,6, .....46
+        new Random().ints(1, 46)
+                .distinct()
+                .limit(6)
+                .forEach(System.out::println);
+                
+
+//                .forEach(System.out::println);
+//                .collect(Collectors.toList());
+    }
+
 
     // 5.4 두 개의 주사위를 굴려서 나온 눈의 합이 6인 경우를 모두 출력하시오.
     public List<Integer[]> quiz4() {
