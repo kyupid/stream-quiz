77f9d8b255329a7623dc39ffdafbb20e378be190
kyupid <iamscvv@gmail.com>
2023-01-29 11:28:59 +0900

    quiz2.1,2.2

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.*;
 
 public class Quiz2 {
 
@@ -12,13 +13,19 @@ public class Quiz2 {
     // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
     // ex) ("T", 1), ("a", 2) ...
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+        return WORDS.stream()
+                .map(word -> word.substring(0, 1))
+                .collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
     }
 
-    // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
+    // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만,
+    // 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
     // ex) ["Hello", "a", "Island", "b"] -> “H I”
     public String quiz2() {
-        return null;
+        return WORDS.stream()
+                .filter(word -> word.toCharArray().length > 1)
+                .map(word -> word.toUpperCase().substring(0, 1))
+                .collect(Collectors.joining(" "));
     }
 
 }
